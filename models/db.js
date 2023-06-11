254d68372d1fceeeabb9d513b172f88d2e55e9bf
MatheusSouzaRodrigues235 <matheus.rodrigues245@etec.sp.gov.br>
2023-06-11 18:03:03 -0300

    Realizando conexao com banco remoto

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('db_projetos', 'root', '', { host: 'localhost', dialect: 'mysql' })
+const sequelize = new Sequelize('heroku_529308256408164', 'be5e60c477974a', '3ffcd86c', { host: 'us-cdbr-east-06.cleardb.net', dialect: 'mysql' })
 
 sequelize.authenticate().then(() => {
     console.log("Conexão com o banco de dados realizada com sucesso!")
